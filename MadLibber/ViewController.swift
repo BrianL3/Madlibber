82368677c80c8020cada3c661de18348a03d7007
BrianL3 <Brian.James.Ledbetter@gmail.com>
2014-10-17 14:12:04 -0700

    button to flip words in the array is now working

--- MadLibber/ViewController.swift
+++ MadLibber/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
-    
+/*
+extension Array {
+        func randomItem() -> T {
+            let index = Int(arc4random_uniform(UInt32(self.count)))
+            return self.index
+    }
+*/
     var noun:String = "Money"
     var verb:String = "Verb"
     var emoji:String = "Emoji"
+    var nounArray:[String] = ["Jim", "Bob", "Andy", "Alice", "Candy"]
+    var verbArray:[String] = ["sings", "dances", "programs", "eats"]
+    var emojiArray:[String] = ["happy", "good", "pumped", "rad"]
     var madlib:String = "When X Yd, he felt Z"
     @IBOutlet weak var nounLabel: UILabel!
     @IBOutlet weak var verbLabel: UILabel!
@@ -30,8 +41,18 @@ class ViewController: UIViewController {
         emojiLabel.text = String(emoji)
     }
     
+    @IBAction func nounSwitch(){
+        println("calling randomIndexer")
+        noun = nounArray[randomIndexer(nounArray)]
+        updateLabels()
+    }
+
     
-    
+    func randomIndexer(parameterArray: [String]) -> Int{
+        let randomIndex:Int = Int(arc4random_uniform(UInt32(parameterArray.count)))
+        println(randomIndex)
+        return randomIndex
+    }
     
     
     // unimportant
